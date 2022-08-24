f9fc6639e27775b85e230637dd963781ba1ed288
Drew Taylor <drew@chainofinsight.com>
2022-08-24 12:25:59 -0400

    fix infura

--- vuejs/nft-basic/src/ipfs.js
+++ vuejs/nft-basic/src/ipfs.js
@@ -1,9 +1,28 @@
 import { create, urlSource } from 'ipfs-http-client';
 
-const ipfs = create('https://ipfs.infura.io:5001');
+const INFURA_PROJECT = process.env.VUE_APP_INFURA_PROJECT_ID;
+const INFURA_API_SECRET = process.env.VUE_APP_INFURA_API_KEY;
+
+const AuthHeader = 'Basic ' + Buffer.from(INFURA_PROJECT + ":" + INFURA_API_SECRET).toString('base64');
 const ipfsGateway = 'https://ipfs.io/ipfs/';
 
 const upload = async function (uploadPath) {
+
+  console.log("Infura?", {
+    INFURA_PROJECT: INFURA_PROJECT,
+    INFURA_API_SECRET: INFURA_API_SECRET,
+    AuthHeader: AuthHeader
+  });
+
+  const ipfs = await create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+      'Authorization': AuthHeader
+    }
+  });
+
   const file = await ipfs.add(urlSource(uploadPath));
   return file;
 };
