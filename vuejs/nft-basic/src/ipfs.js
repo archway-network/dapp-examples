b5b4543ce8ab5f36c182961d1e1ad0ebb9b41108
Drew Taylor <drew@chainofinsight.com>
2022-08-24 12:43:56 -0400

    Switch IPFS gateway resolver

--- vuejs/nft-basic/src/ipfs.js
+++ vuejs/nft-basic/src/ipfs.js
@@ -4,7 +4,7 @@ const INFURA_PROJECT = process.env.VUE_APP_INFURA_PROJECT_ID;
 const INFURA_API_SECRET = process.env.VUE_APP_INFURA_API_KEY;
 
 const AuthHeader = 'Basic ' + Buffer.from(INFURA_PROJECT + ":" + INFURA_API_SECRET).toString('base64');
-const ipfsGateway = 'https://ipfs.io/ipfs/';
+const ipfsGateway = 'https://cloudflare-ipfs.com/ipfs/';
 
 const upload = async function (uploadPath) {
 
